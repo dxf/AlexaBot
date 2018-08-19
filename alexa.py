b1692dd0234403fd7812609e284052b9940100c6
RealHeroicGamer <38766755+RealHeroicGamer@users.noreply.github.com>
2018-08-19 16:06:41 -0500

    added linebreak, fixed status and help

--- alexa.py
+++ alexa.py
@@ -2,7 +2,8 @@ token = input('Enter your bot token here! No quotes!')
 import discord
 import asyncio
 nowplay = "ɴᴏᴡ ᴘʟᴀʏɪɴɢ: "
-status = " ────────────────:white_circle:────────────────── ◄◄⠀▐▐ ⠀►►\n───○ 🔊ᴴᴰ ⚙️ ❐ ⊏⊐"
+linebreak = "\u000A"
+status = " ───────────:white_circle:────── ◄◄⠀▐▐ ⠀►►⠀⠀ ⠀ 𝟸:𝟷𝟾 / 𝟹:𝟻𝟼 ⠀ ─○─ :loud_sound:⠀ ᴴᴰ :gear:️ ❐ ⊏⊐"
 client = discord.Client()
 @client.event
 async def on_ready():
@@ -12,14 +13,14 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('Alexa play'):
         song = message.content[11:]
-        finalcontent = nowplay + song + status
+        finalcontent = nowplay + song + linebreak + status
         await client.send_message(message.channel, finalcontent)
     if message.content.startswith('Alexa, play'):
         songcomma = message.content[12:]
-        finalcontentcomma = nowplay + songcomma + status
+        finalcontentcomma = nowplay + songcomma + linebreak + status
         await client.send_message(message.channel, finalcontentcomma)
     if message.content.startswith('Alexa help'):
-        await client.send_message(message.channel, "Summon me by saying \"Alexa, play songname\" or \"Alexa play songname\".")
+        await client.send_message(message.channel, "Summon me by saying \"Alexa, play [song]\" or \"Alexa play [song]\".")
     if message.content.startswith('Alexa, help'):
-        await client.send_message(message.channel, "Summon me by saying \"Alexa, play songname\" or \"Alexa play songname\".")
+        await client.send_message(message.channel, "Summon me by saying \"Alexa, play [song]\" or \"Alexa play [song]\".")
 client.run(token)
